b00362feb3af65dd4cefbc550f1130cdd5e5a44e
Oscar Rosario <oscaralbertorosariogenao@gmail.com>
2026-04-18 11:26:18 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,40 @@
-const CACHE_NAME = "loteka-v1";
+const CACHE_NAME = "loteka-app-v1";
 
+// Archivos básicos a cachear
+const urlsToCache = [
+  "/",
+  "/app-reportes.html",
+  "/manifest.json"
+];
+
+// 🔹 INSTALACIÓN
 self.addEventListener("install", event => {
+  console.log("Service Worker instalado");
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll([
-        "/",
-        "/index.html",
-        "/manifest.json"
-      ]);
+      return cache.addAll(urlsToCache);
     })
   );
 });
 
+// 🔹 ACTIVACIÓN
+self.addEventListener("activate", event => {
+  console.log("Service Worker activado");
+
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// 🔹 FETCH (manejo de solicitudes)
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
