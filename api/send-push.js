be8655a25aa28042d667124842eba4cce78cde7e
Oscar Rosario <oscaralbertorosariogenao@gmail.com>
2026-04-20 13:31:53 -0400

    Update send-push.js

--- api/send-push.js
+++ api/send-push.js
@@ -1,10 +1,6 @@
-import { createClient } from "@supabase/supabase-js";
-
 export default function handler(req, res) {
-  return res.status(200).json({
+  res.status(200).json({
     ok: true,
-    message: "IMPORT OK",
-    hasUrl: !!process.env.SUPABASE_URL,
-    hasKey: !!process.env.SUPABASE_SERVICE_ROLE_KEY
+    message: "CAMBIO-777"
   });
 }
