7defc0966292f9fb5944f959c9422d8ba4f97efe
Oscar Rosario <oscaralbertorosariogenao@gmail.com>
2026-05-19 11:34:04 -0400

    Update probe.js

--- api/probe.js
+++ api/probe.js
@@ -1,4 +1,4 @@
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   res.statusCode = 200;
   res.setHeader("Content-Type", "application/json; charset=utf-8");
   res.end(JSON.stringify({
@@ -6,4 +6,4 @@ module.exports = async function handler(req, res) {
     message: "API LOTEKA activo en Vercel",
     time: new Date().toISOString()
   }));
-};
+}
