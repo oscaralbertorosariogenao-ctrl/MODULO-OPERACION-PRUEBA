9cc24a9598a5fcf9318a27e0ba636f4f97cfe848
Oscar Rosario <oscaralbertorosariogenao@gmail.com>
2026-07-27 08:08:33 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -11,7 +11,7 @@
    - Sin skipWaiting agresivo ni clients.claim automático.
    ========================================================================== */
 
-const SW_VERSION = "2026-07-25-v805.12-pwa-scanner";
+const SW_VERSION = "2026-07-25-v805.13-pwa-scanner";
 
 const STATIC_CACHE = `loteka-static-${SW_VERSION}`;
 const HTML_CACHE = `loteka-html-${SW_VERSION}`;
