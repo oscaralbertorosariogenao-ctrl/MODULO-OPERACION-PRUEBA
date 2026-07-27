018e9fc0ae2ab2ef2c26fe046e0ae6d26a69d270
Oscar Rosario <oscaralbertorosariogenao@gmail.com>
2026-07-27 13:49:06 -0400

    Update app-frame-view.js

--- assets/app/js/views/app-frame-view.js
+++ assets/app/js/views/app-frame-view.js
@@ -4,5 +4,5 @@ import { bottomNavigation } from '../components/bottom-navigation.js';
 import { appDrawer } from '../components/app-drawer.js';
 import { offlineBanner } from '../components/offline-banner.js';
 export function appFrameView(state, content){
-  return el('div',{class:'app-frame'},appHeader(state),offlineBanner(state.connectivity),el('main',{class:'app-main',id:'app-view',tabindex:'-1'},el('div',{class:'app-view'},content)),bottomNavigation(state.route.path),appDrawer(state));
+  return el('div',{class:'app-frame'},appHeader(state),offlineBanner(state.connectivity),el('main',{class:'app-main',id:'app-view',tabindex:'-1'},el('div',{class:'app-view'},content)),bottomNavigation(state),appDrawer(state));
 }
