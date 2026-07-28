7542ada1d5511ba223d1b34ef41d99f5fcfcc99f
Oscar Rosario <oscaralbertorosariogenao@gmail.com>
2026-07-28 14:02:23 -0400

    Update config.js

--- assets/app/js/config.js
+++ assets/app/js/config.js
@@ -1,5 +1,5 @@
-export const APP_BUILD = '2026-07-28-v805.21-entrada-rapida-encargado';
-export const APP_VERSION = 'V805.21';
+export const APP_BUILD = '2026-07-28-v805.22-scanner-contextual-encargado';
+export const APP_VERSION = 'V805.22';
 export const SUPABASE_URL = 'https://tnymrjxdhzdmpcbilftj.supabase.co';
 export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRueW1yanhkaHpkbXBjYmlsZnRqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgyNjEwOTksImV4cCI6MjA5MzgzNzA5OX0.YXG9juChbJUUdsdy01Qkoh9X0-MijewD5aQbKnG9Itk';
 export const R2_UPLOAD_ENDPOINT = '/api/r2-upload';
