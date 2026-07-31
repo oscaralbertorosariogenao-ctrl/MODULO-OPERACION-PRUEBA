4d8b5b5f9758dfe108bea25053bf72339883f484
Oscar Rosario <oscaralbertorosariogenao@gmail.com>
2026-07-31 15:48:20 -0400

    Update service worker version for cache management

--- service-worker.js
+++ service-worker.js
@@ -11,7 +11,7 @@
    - Sin skipWaiting agresivo ni clients.claim automático.
    ========================================================================== */
 
-const SW_VERSION = "2026-07-31-v807.01-carga-grupos-levantamientos";
+const SW_VERSION = "2026-07-31-v807.02-creacion-levantamientos";
 
 const STATIC_CACHE = `loteka-static-${SW_VERSION}`;
 const HTML_CACHE = `loteka-html-${SW_VERSION}`;
