e742bbca417af3d0b97f6b2a3f029d16e75f2fd1
Oscar Rosario <oscaralbertorosariogenao@gmail.com>
2026-08-07 08:41:59 -0400

    Update app build and version in config.js

--- assets/app/js/config.js
+++ assets/app/js/config.js
@@ -1,5 +1,5 @@
-export const APP_BUILD = '2026-08-06-v808.20-reportes-unificados';
-export const APP_VERSION = 'V808.20';
+export const APP_BUILD = '2026-08-06-v808.21-correcciones-criticas';
+export const APP_VERSION = 'V808.21';
 export const SUPABASE_URL = 'https://tnymrjxdhzdmpcbilftj.supabase.co';
 export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRueW1yanhkaHpkbXBjYmlsZnRqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgyNjEwOTksImV4cCI6MjA5MzgzNzA5OX0.YXG9juChbJUUdsdy01Qkoh9X0-MijewD5aQbKnG9Itk';
 export const R2_UPLOAD_ENDPOINT = '/api/r2-upload';
