69284f9b862659e558c1aa5662422615ea8f8bac
Oscar Rosario <oscaralbertorosariogenao@gmail.com>
2026-08-18 14:21:54 -0400

    Update config.js

--- assets/app/js/config.js
+++ assets/app/js/config.js
@@ -1,5 +1,5 @@
-export const APP_BUILD = '2026-08-13-v808.37-operaciones-domain';
-export const APP_VERSION = 'V808.37';
+export const APP_BUILD = '2026-08-14-v808.38-levantamientos-historial-grupos';
+export const APP_VERSION = 'V808.38';
 export const SUPABASE_URL = 'https://tnymrjxdhzdmpcbilftj.supabase.co';
 export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRueW1yanhkaHpkbXBjYmlsZnRqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgyNjEwOTksImV4cCI6MjA5MzgzNzA5OX0.YXG9juChbJUUdsdy01Qkoh9X0-MijewD5aQbKnG9Itk';
 export const R2_UPLOAD_ENDPOINT = '/api/r2-upload';
